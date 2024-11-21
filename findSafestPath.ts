fa415cc7537fe76de573507a44bc40a4f794ee10
StanTheMackiar <callestanly@gmail.com>
2024-11-21 16:21:02 -0500

    optimized findSafestPath

--- findSafestPath.ts
+++ findSafestPath.ts
@@ -1,63 +1,77 @@
-const defaultDream = [
-  [1, 3, 1],
-  [1, 5, 1],
-  [4, 2, 1]
-];
 
 console.time("findSafestPath");
 
-function findSafestPath(sheet = defaultDream): number {
-  const potentialRoutes: number[][][] = [];
+function findSafestPath(dream: number[][]): number {
+  const potentialRoutes: { total: number; path: number[][] }[] = [];
+  let betterRouteIndex = 0;
 
-  for (let y = 0; y < sheet.length; y++) {
-    const row = sheet[y];
+  for (let y = 0; y < dream.length; y++) {
+    const row = dream[y];
 
     for (let x = 0; x < row.length; x++) {
       const isAtStart = y === 0 && x === 0;
-      if (isAtStart) potentialRoutes.push([[y, x]]);
 
-      const hasRightNeighbor = Boolean(row[x + 1]);
-      const hasBelowNeighbor = Boolean(sheet[y + 1]?.[x]);
+      if (isAtStart)
+        potentialRoutes.push({
+          total: row[x],
+          path: [[y, x]]
+        });
+
+      const rightNeighbor: number | undefined = row[x + 1];
+      const belowNeighbor: number | undefined = dream[y + 1]?.[x];
+
+      const hasRightNeighbor = rightNeighbor !== undefined;
+      const hasBelowNeighbor = belowNeighbor !== undefined;
+
       const rightNeighborCoords = [y, x + 1];
       const belowNeighborCoords = [y + 1, x];
 
       for (let i = 0; i < potentialRoutes.length; i++) {
         const currentRoute = potentialRoutes[i];
 
-        const finalCoordsIndex = currentRoute.length - 1;
-        const [finalCoordY, finalCoordX] = currentRoute[finalCoordsIndex];
+        const finalCoordsIndex = currentRoute.path.length - 1;
+        const [finalCoordY, finalCoordX] = currentRoute.path[finalCoordsIndex];
         const isAtDestinationCell = finalCoordY === y && finalCoordX === x;
 
         if (!isAtDestinationCell) continue;
 
         //?Amplia las rutas colocando los vecinos, si existen dos caminos posibles, se bifurca creando una nueva ruta con todos los nodos anteriores y el vecino
         if (hasRightNeighbor && hasBelowNeighbor) {
-          potentialRoutes.push([...currentRoute, belowNeighborCoords]);
-          currentRoute.push(rightNeighborCoords);
+          potentialRoutes.push({
+            path: [...currentRoute.path, belowNeighborCoords],
+            total: currentRoute.total + belowNeighbor
+          });
+
+          currentRoute.path.push(rightNeighborCoords);
+          currentRoute.total += rightNeighbor;
         }
 
         if (!hasBelowNeighbor && hasRightNeighbor) {
-          currentRoute.push(rightNeighborCoords);
+          currentRoute.path.push(rightNeighborCoords);
+          currentRoute.total += rightNeighbor;
         }
 
         if (!hasRightNeighbor && hasBelowNeighbor) {
-          currentRoute.push(belowNeighborCoords);
+          currentRoute.path.push(belowNeighborCoords);
+          currentRoute.total += belowNeighbor;
         }
+
+        const betterRouteTotal = potentialRoutes[betterRouteIndex].total;
+        betterRouteIndex =
+          currentRoute.total < betterRouteTotal ? i : betterRouteIndex;
       }
     }
   }
 
-  const betterRoute = potentialRoutes.reduce((acc, route) => {
-    const routeSum = route.reduce((acc, [y, x]) => acc + sheet[y][x], 0);
-
-    return routeSum < acc ? routeSum : acc;
-  }, Infinity);
-
-  console.log(potentialRoutes);
-
-  return betterRoute;
+  return potentialRoutes[betterRouteIndex].total;
 }
 
-
 console.timeEnd("findSafestPath");
-console.log(findSafestPath());
+
+const defaultDream = [
+  [1, 3, 1],
+  [1, 5, 1],
+  [4, 2, 1]
+];
+
+console.log(findSafestPath(defaultDream));
